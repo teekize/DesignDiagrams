5c47ceff275fb20955b103d2fe079e569a212217
teekize <elvisteeka1@gmail.com>
2020-01-04 14:10:30 +0300

    used the grid system to place objects on the window

--- hello.py
+++ hello.py
@@ -2,9 +2,23 @@ from tkinter import *
 
 root = Tk()
 
-#we are creating a lable widget
+"""
+we are creating a lable widget
+"""
 myLabel = Label(root, text="Hello wolrd")
+myLabel2 = Label(root, text="my name is Teeka")
 #then we place it on the root 
-myLabel.pack()
 
+""" 
+this is the grid system where things are arranged in rows and columns
+with this grid we want to place the widget on the screen
+"""
+
+myLabel.grid(row=0, column=0)
+myLabel2.grid(row=0, column=1)
+
+"""
+the eventloop. it does things as long as the event is still happaning
+its in constant loop until its interrupted.
+"""
 root.mainloop()
