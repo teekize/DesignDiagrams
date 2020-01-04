1708a93c3d60fa584f9c6d918cf1fa29ab27db56
teekize <elvisteeka1@gmail.com>
2020-01-04 13:56:01 +0300

    created the first lable widget in tkinter

---
+++ hello.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+#we are creating a lable widget
+myLabel = Label(root, text="Hello wolrd")
+#then we place it on the root 
+myLabel.pack()
+
+root.mainloop()
